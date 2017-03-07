292ef41d1fe4102be09831f2fa63432f05abf23e
Joel Rorseth <joelrorseth@gmail.com>
2017-03-07 11:17:02 -0500

    Split up sign up and sign in views

--- at.me/SignInViewController.swift
+++ at.me/SignInViewController.swift
@@ -51,94 +51,94 @@ class SignInViewController: UIViewController {
     
     // ==========================================
     // ==========================================
-    @IBAction func didTapSignUp(_ sender: Any) {
-        
-        // Make sure all important info is provided, then store
-        guard let email = emailField.text, let password = passwordField.text else { return }
-        let username = (self.usernameField.text == nil) ? email.components(separatedBy: "@")[0] : self.usernameField.text!
-
-        
-        
-        // ERROR CASE 1: Weak password
-        if (password.characters.count < 6) {
-            
-            // Alert user that their password needs to be stronger
-            let ac = UIAlertController(title: "Password Is Too Weak", message: "Your password must be 6 or more characters.", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            
-            // Present alert, then shift focus to password field
-            self.present(ac, animated: true) { self.passwordField.becomeFirstResponder() }
-            return
-        }
-        
-        // ERROR CASE 2: Improper username specified at signup
-        // TODO: Look into restrictions on <FIRDataSnapshot>.hasChild() which will affect valid usernames
-        if (username.characters.count < 4 || username.contains(" ")) {
-            
-            // Alert user that their username needs to be better
-            let ac = UIAlertController(title: "Username is Invalid", message: "Your username must be 4 or more valid characters", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            
-            // Present alert, then shift focus to username field
-            self.present(ac, animated: true) { self.usernameField.becomeFirstResponder() }
-            return
-        }
-        
-        
-        // Take snapshot of databse to check for existing username
-        registeredUsernamesRef.observeSingleEvent(of: FIRDataEventType.value, with: { (snapshot) in
-            
-            // If username is not found, we are OK to create account
-            if (!snapshot.hasChild(username)) {
-                
-                
-                // Let the auth object create a user with given fields
-                FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
-                    
-                    
-                    // ERROR CASE 2: Any other error (Email taken)
-                    if let error = error {
-                        print("<<<< AT.ME::DEBUG >>>>: \(error.localizedDescription)")
-                        return
-                    }
-                    
-                    // Add entry to database with public user information (username, email)
-                    // TODO: Link up text fields for name, display name etc in a separate controller
-                    let userEntry = [
-                        "displayName" : username,
-                        "email" : email,
-                        "firstName" : "FNAME",
-                        "lastName" : "LNAME",
-                        "username" : username,
-                    ]
-                    
-                    
-                    // Add entry to usernames registry and user info registry
-                    self.registeredUsernamesRef.child(username).setValue((user?.uid)!)
-                    self.userInformationRef.child((user?.uid)!).setValue(userEntry)
-                    
-                    // Update any <FIRUser> properties maintained internally by Firebase
-                    let changeRequest = user?.profileChangeRequest()
-                    changeRequest?.displayName = username
-                    
-                    
-                    // First time use, set up user name then log into app
-                    print("<<<< AT.ME::DEBUG >>>>:: New user creation successful")
-                    self.setAccountDetails(user!)
-                    self.didTapSignIn(self)
-                }
- 
-                
-            } else {
-                
-                // ERROR CASE 3: Username was already taken
-                // Alert user that username already exists, shift focus to username field
-                let ac = UIAlertController(title: "Username Already Taken", message: "Please choose another username.", preferredStyle: .alert)
-                ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                self.present(ac, animated: true) { self.usernameField.becomeFirstResponder() }
-            }
-        })
-    }
+//    @IBAction func didTapSignUp(_ sender: Any) {
+//        
+//        // Make sure all important info is provided, then store
+//        guard let email = emailField.text, let password = passwordField.text else { return }
+//        let username = (self.usernameField.text == nil) ? email.components(separatedBy: "@")[0] : self.usernameField.text!
+//
+//        
+//        
+//        // ERROR CASE 1: Weak password
+//        if (password.characters.count < 6) {
+//            
+//            // Alert user that their password needs to be stronger
+//            let ac = UIAlertController(title: "Password Is Too Weak", message: "Your password must be 6 or more characters.", preferredStyle: .alert)
+//            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//            
+//            // Present alert, then shift focus to password field
+//            self.present(ac, animated: true) { self.passwordField.becomeFirstResponder() }
+//            return
+//        }
+//        
+//        // ERROR CASE 2: Improper username specified at signup
+//        // TODO: Look into restrictions on <FIRDataSnapshot>.hasChild() which will affect valid usernames
+//        if (username.characters.count < 4 || username.contains(" ")) {
+//            
+//            // Alert user that their username needs to be better
+//            let ac = UIAlertController(title: "Username is Invalid", message: "Your username must be 4 or more valid characters", preferredStyle: .alert)
+//            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//            
+//            // Present alert, then shift focus to username field
+//            self.present(ac, animated: true) { self.usernameField.becomeFirstResponder() }
+//            return
+//        }
+//        
+//        
+//        // Take snapshot of databse to check for existing username
+//        registeredUsernamesRef.observeSingleEvent(of: FIRDataEventType.value, with: { (snapshot) in
+//            
+//            // If username is not found, we are OK to create account
+//            if (!snapshot.hasChild(username)) {
+//                
+//                
+//                // Let the auth object create a user with given fields
+//                FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
+//                    
+//                    
+//                    // ERROR CASE 2: Any other error (Email taken)
+//                    if let error = error {
+//                        print("<<<< AT.ME::DEBUG >>>>: \(error.localizedDescription)")
+//                        return
+//                    }
+//                    
+//                    // Add entry to database with public user information (username, email)
+//                    // TODO: Link up text fields for name, display name etc in a separate controller
+//                    let userEntry = [
+//                        "displayName" : username,
+//                        "email" : email,
+//                        "firstName" : "FNAME",
+//                        "lastName" : "LNAME",
+//                        "username" : username,
+//                    ]
+//                    
+//                    
+//                    // Add entry to usernames registry and user info registry
+//                    self.registeredUsernamesRef.child(username).setValue((user?.uid)!)
+//                    self.userInformationRef.child((user?.uid)!).setValue(userEntry)
+//                    
+//                    // Update any <FIRUser> properties maintained internally by Firebase
+//                    let changeRequest = user?.profileChangeRequest()
+//                    changeRequest?.displayName = username
+//                    
+//                    
+//                    // First time use, set up user name then log into app
+//                    print("<<<< AT.ME::DEBUG >>>>:: New user creation successful")
+//                    self.setAccountDetails(user!)
+//                    self.didTapSignIn(self)
+//                }
+// 
+//                
+//            } else {
+//                
+//                // ERROR CASE 3: Username was already taken
+//                // Alert user that username already exists, shift focus to username field
+//                let ac = UIAlertController(title: "Username Already Taken", message: "Please choose another username.", preferredStyle: .alert)
+//                ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//                self.present(ac, animated: true) { self.usernameField.becomeFirstResponder() }
+//            }
+//        })
+//    }
     
     
     // MARK: View
