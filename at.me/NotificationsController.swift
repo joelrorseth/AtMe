78db8f8d2e907dbf926cc252159fb54f7873514f
Joel Rorseth <joelrorseth@gmail.com>
2017-07-17 20:58:58 -0400

    Improve documentation for NotificationsController

--- at.me/NotificationsController.swift
+++ at.me/NotificationsController.swift
@@ -10,8 +10,14 @@ import OneSignal
 
 class NotificationsController {
     
-    // ==========================================
-    // ==========================================
+    
+    /**
+     Send a push notification to a specified user
+     - parameters:
+        - userID: The userID of the user whom you intend to deliver the notification to
+        - title: The title of the push notification. This should be the name of the sender.
+        - message: The message to be displayed in the push notification. This should be the text of a message.
+     */
     public static func send(to userID: String, title: String, message: String) {
      
         let jsonMessage: [AnyHashable: Any]! = ["contents": ["en": message],
@@ -19,15 +25,15 @@ class NotificationsController {
             "ios_badgeType": "Increase", "ios_badgeCount": 1]
         
         OneSignal.postNotification(jsonMessage, onSuccess: { _ in
-            // TODO: Implement callback / UI update to show "delivered"
+            // TODO: In future update, implement callback / UI update to show "delivered"
             
         }, onFailure: { _ in
             print("Notification could not be delivered")
         })
     }
     
-    // ==========================================
-    // ==========================================
+    
+    /** Obtain the notificationID of the current authorized user */
     public static func currentUserNotificationsID() -> String? {
         
         let status: OSPermissionSubscriptionState = OneSignal.getPermissionSubscriptionState()
