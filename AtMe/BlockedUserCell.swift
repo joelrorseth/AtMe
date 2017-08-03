a0b50c43c6b646befa7d1ef0b7b203825517fe30
Joel Rorseth <joelrorseth@gmail.com>
2017-08-03 16:03:40 -0400

    Add delegation and handle unblocking users from the blocked users table

--- AtMe/BlockedUserCell.swift
+++ AtMe/BlockedUserCell.swift
@@ -8,16 +8,26 @@
 
 import UIKit
 
+// Protocol defines methods that will be called in response to interactions in the cell
+protocol BlockedUserCellDelegate {
+    func didTapUnblock(button: UIButton, uid: String, username: String)
+}
+
+
 class BlockedUserCell: UITableViewCell {
+    
+    var blockUserCellDelegate: BlockedUserCellDelegate?
+    var uid: String?
+    var username: String?
     
     @IBOutlet var displayPictureImageView: UIImageView!
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var usernameLabel: UILabel!
     @IBOutlet var unblockButton: UIButton!
     
-    @IBAction func didTapUnblockButton(_ sender: Any) {
+    @IBAction func didTapUnblockButton(_ sender: UIButton) {
+        
+        guard let uid = self.uid, let username = self.username else { return }
+        blockUserCellDelegate?.didTapUnblock(button: sender, uid: uid, username: username)
     }
-    
-    var uid: String?
-    var username: String?
 }
--- AtMe/BlockedUsersViewController.swift
+++ AtMe/BlockedUsersViewController.swift
@@ -31,7 +31,7 @@ class BlockedUsersViewController: UITableViewController {
         self.title = "Blocked"
         tableView.tableFooterView = UIView()
     }
-
+    
 
     // MARK: - Table view
     /**
@@ -56,6 +56,8 @@ class BlockedUsersViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.CellIdentifiers.blockedUserCell, for: indexPath) as! BlockedUserCell
 
+        // Important: Set delegate for button so this view controller knows about taps
+        cell.blockUserCellDelegate = self
         cell.unblockButton.layer.cornerRadius = Constants.Radius.regularRadius
         
         // Extract user info from relevant data source record
@@ -65,6 +67,12 @@ class BlockedUsersViewController: UITableViewController {
         let uid = blockedUsers[indexPath.row].uid
         let path = "displayPictures/\(uid)/\(uid).JPG"
         
+        
+        // Also important: Set uid and username for use in the cell delegate
+        cell.uid = uid
+        cell.username = blockedUsers[indexPath.row].username
+        
+        
         // Download the image into the display picture image view
         DatabaseController.downloadImage(into: cell.displayPictureImageView, from: path, completion: { _ in
             cell.displayPictureImageView.layer.masksToBounds = true
@@ -72,5 +80,35 @@ class BlockedUsersViewController: UITableViewController {
         })
 
         return cell
+    }
+}
+
+extension BlockedUsersViewController: BlockedUserCellDelegate {
+ 
+    /**
+     Handle unblocking the selected user and removing them from the table.
+     - parameters:
+        - button: A reference to the button which was selected to trigger this change.
+        - uid: The uid of the user selected to be unblocked.
+        - username: The username of the user selected to be unblocked.
+    */
+    func didTapUnblock(button: UIButton, uid: String, username: String) {
+        
+        UIView.animate(withDuration: 0.2, animations: {
+            button.backgroundColor = UIColor.groupTableViewBackground
+            button.alpha = 0.0
+        
+        }, completion: { _ in
+
+            // Unblock the user and remove that row from table and data source
+            AuthController.unblockUser(uid: uid, username: username)
+            
+            for (index, user) in self.blockedUsers.enumerated() {
+                if (uid == user.uid) {
+                    self.blockedUsers.remove(at: index)
+                    self.tableView.deleteRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
+                }
+            }
+        })
     }
 }
