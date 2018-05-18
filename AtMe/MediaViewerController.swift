3ca9297236ed17f9a9952d58e7f0da95df96d1b9
Joel Rorseth <joelrorseth@gmail.com>
2018-05-17 22:48:11 -0400

    Implement pinch to zoom in media viewer

--- AtMe/MediaViewerController.swift
+++ AtMe/MediaViewerController.swift
@@ -9,33 +9,50 @@
 import UIKit
 
 class MediaViewerController: UIViewController {
-
-    var contentImageView: UIImageView!
-    var image: UIImage?
+  
+  var scrollView: UIScrollView!
+  var contentImageView: UIImageView!
+  var image: UIImage?
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        view.backgroundColor = UIColor.black
-        
-        contentImageView = UIImageView(frame: self.view.frame)
-        contentImageView.contentMode = UIViewContentMode.scaleAspectFit
-        view.addSubview(contentImageView)
-        
-//        contentImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-//        contentImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-//        contentImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-//        contentImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        
-        if let image = image {
-            contentImageView.image = image
-        } else { print("Error: No image provided to MediaViewerController") }
-
-        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(MediaViewerController.viewTapped))
-        view.addGestureRecognizer(tapRecognizer)
-    }
+    view.backgroundColor = UIColor.black
     
-    @objc func viewTapped() {
-        dismiss(animated: true, completion: nil)
-    }
+    scrollView = UIScrollView(frame: view.frame)
+    scrollView.minimumZoomScale = 1.0
+    scrollView.maximumZoomScale = 5.0
+    scrollView.delegate = self
+    
+    contentImageView = UIImageView(frame: self.view.frame)
+    contentImageView.contentMode = UIViewContentMode.scaleAspectFit
+    
+    // Form view hierarchy -- UIView -> UIScrollView -> UIImageView
+    view.addSubview(scrollView)
+    scrollView.addSubview(contentImageView)
+    
+    // Provide the UIImage to the UIImageView
+    if let image = image {
+      contentImageView.image = image
+    } else { print("Error: No image provided to MediaViewerController") }
+    
+    let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(MediaViewerController.viewTapped))
+    view.addGestureRecognizer(tapRecognizer)
+  }
+
+  // Handler for tap gesture on the view
+  @objc func viewTapped() {
+    dismiss(animated: true, completion: nil)
+  }
+}
+
+
+// MARK: UIScrollViewDelegate
+extension MediaViewerController: UIScrollViewDelegate {
+  
+  // Provide the view to be scaled when zooming in the UIScrollView
+  func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+    return contentImageView
+  }
 }
