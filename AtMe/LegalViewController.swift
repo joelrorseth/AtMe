1fa8a889d9eefe6bde57d72a42d4a41fb965ab3a
Joel Rorseth <joelrorseth@gmail.com>
2018-06-04 07:35:01 -0700

    Fix LegalViewController navigation bar spacing bug

--- AtMe/LegalViewController.swift
+++ AtMe/LegalViewController.swift
@@ -9,61 +9,68 @@
 import UIKit
 
 class LegalViewController: UIViewController {
+  
+  
+  @IBOutlet var segmentedControl: UISegmentedControl!
+  @IBOutlet var mainTextView: UITextView!
+  
+  var terms: String = ""
+  var privacy: String = "Privacy"
+  
+  
+  // MARK: - Storyboard Controls Actions
+  /** Called when done button is pressed inside view controller. */
+  @IBAction func didPressDoneButton(_ sender: Any) {
+    self.dismiss(animated: true, completion: nil)
+  }
+  
+  
+  /** Called when the segmented control changed it's selected value. */
+  @IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+    if (sender.titleForSegment(at: sender.selectedSegmentIndex) == "Terms of Service") {
+      mainTextView.text = terms
+    } else {
+      mainTextView.text = privacy
+    }
+  }
+  
+  
+  // MARK: - View
+  /**
+   Overridden variable to set the style (color) of the status bar
+   */
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return UIStatusBarStyle.lightContent
+  }
+  
+  
+  /** Method called when view controller has been loaded */
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.title = "Legal"
+    
+    // EXtract the policies from the included text files
+    if let path = Bundle.main.path(forResource: "terms", ofType: "txt") {
+      
+      do { try terms = String(contentsOfFile: path) }
+      catch _ as Error? {}
+    }
+    
+    if let path = Bundle.main.path(forResource: "privacy", ofType: "txt") {
+      
+      do { try privacy = String(contentsOfFile: path) }
+      catch _ as Error? {}
+    }
+    
+    // Force statement load
+    segmentedControlValueChanged(segmentedControl)
+  }
+}
 
-    
-    @IBOutlet var segmentedControl: UISegmentedControl!
-    @IBOutlet var mainTextView: UITextView!
-    
-    var terms: String = ""
-    var privacy: String = "Privacy"
-    
-    
-    // MARK: - Storyboard Controls Actions
-    /** Called when done button is pressed inside view controller. */
-    @IBAction func didPressDoneButton(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
-    }
-
-    
-    /** Called when the segmented control changed it's selected value. */
-    @IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
-        if (sender.titleForSegment(at: sender.selectedSegmentIndex) == "Terms of Service") {
-            mainTextView.text = terms
-        } else {
-            mainTextView.text = privacy
-        }
-    }
-    
-    
-    // MARK: - View
-    /**
-     Overridden variable to set the style (color) of the status bar
-    */
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return UIStatusBarStyle.lightContent
-    }
-    
-    
-    /** Method called when view controller has been loaded */
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.title = "Legal"
-        
-        // EXtract the policies from the included text files
-        if let path = Bundle.main.path(forResource: "terms", ofType: "txt") {
-            
-            do { try terms = String(contentsOfFile: path) }
-            catch _ as Error? {}
-        }
-        
-        if let path = Bundle.main.path(forResource: "privacy", ofType: "txt") {
-            
-            do { try privacy = String(contentsOfFile: path) }
-            catch _ as Error? {}
-        }
-        
-        // Force statement load
-        segmentedControlValueChanged(segmentedControl)
-    }
+extension LegalViewController: UIBarPositioningDelegate {
+  
+  func position(for bar: UIBarPositioning) -> UIBarPosition {
+    return UIBarPosition.topAttached
+  }
 }
