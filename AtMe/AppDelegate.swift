3df8d600bca2a3929627e94f2f60fec48363c521
Joel Rorseth <joelrorseth@gmail.com>
2018-06-07 08:09:06 -0700

    Removed deprecated isStatusBarHidden, clean up AppDelegate

--- AtMe/AppDelegate.swift
+++ AtMe/AppDelegate.swift
@@ -13,54 +13,30 @@ import OneSignal
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions
+    launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     
-    var window: UIWindow?
+    UINavigationBar.appearance().barStyle = UIBarStyle.black
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        UIApplication.shared.isStatusBarHidden = false
-        UINavigationBar.appearance().barStyle = UIBarStyle.black
-        
-        let onesignalInitSettings = [kOSSettingsKeyAutoPrompt: false]
-        
-        // Init OneSignal
-        OneSignal.initWithLaunchOptions(launchOptions, appId: Constants.App.oneSignalAppId,
-            handleNotificationAction: nil, settings: onesignalInitSettings)
-        
-        OneSignal.inFocusDisplayType = OSNotificationDisplayType.notification
-        
-        // Recommend moving the below line to prompt for push after informing the user about how your app will use them.
-        OneSignal.promptForPushNotifications(userResponse: { accepted in
-            print("AtMe:: User accepted notifications: \(accepted)")
-        })
-        
-        // Enable offline data persistence to cache selected observed data and authenticated user
-        FirebaseApp.configure()
-        Database.database().isPersistenceEnabled = true
-        
-        return true
-    }
+    let onesignalInitSettings = [kOSSettingsKeyAutoPrompt: false]
     
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
+    // Init OneSignal
+    OneSignal.initWithLaunchOptions(launchOptions, appId: Constants.App.oneSignalAppId,
+                                    handleNotificationAction: nil, settings: onesignalInitSettings)
     
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
+    OneSignal.inFocusDisplayType = OSNotificationDisplayType.notification
     
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
+    // Recommend moving the below line to prompt for push after informing the user about how your app will use them.
+    OneSignal.promptForPushNotifications(userResponse: nil)
     
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        
-    }
+    // Enable offline data persistence to cache selected observed data and authenticated user
+    FirebaseApp.configure()
+    Database.database().isPersistenceEnabled = true
     
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
+    return true
+  }
 }
